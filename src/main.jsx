98e18dc536eef0d55d5d635a69bd3d801c4abef6
HANA0K4 <mateohanaoka@gmail.com>
2023-02-26 16:01:52 -0500

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Categories } from './Components/Categories'
-import './styleCategories.css'
+import GifGrid from './Components/GifGrid'
+import './styleGif.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Categories/>
+    <GifGrid category= 'lol'/>
   </React.StrictMode>
 )
