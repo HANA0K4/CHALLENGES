e56f5727e120472741eb28f1a5b4b5d8657c4da2
HANA0K4 <mateohanaoka@gmail.com>
2023-02-26 15:29:05 -0500

    V1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Handle } from './Components/Handle'
-import './styleHandle.css'
+import { Categories } from './Components/Categories'
+import './styleCategories.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Handle value = {1}/>
+    <Categories/>
   </React.StrictMode>
 )
