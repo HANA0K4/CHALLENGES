c5574927b7da40cec8a3612d82a48e7dd7e1db6b
HANA0K4 <mateohanaoka@gmail.com>
2023-04-19 09:39:05 -0500

    V1

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Father } from './hooks/challenge-8/Father'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Father />
+  </React.StrictMode>,
+
+
+)
