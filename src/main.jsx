01c2df54e4db4383ceba257151d12d8ec7a9175e
HANA0K4 <mateohanaoka@gmail.com>
2023-04-19 10:17:58 -0500

    V1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Father } from './hooks/challenge-8/Father'
+import { TodoApp } from './hooks/challenge-9/TodoApp'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Father />
+    <TodoApp />
   </React.StrictMode>,
 
 
